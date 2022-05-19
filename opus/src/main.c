c8d232ed587bda2b01c3bb3e8df413aca5d9a140
Xavier0301 <xavier.servot0@gmail.com>
2022-05-19 15:21:21 +0200

    Fixes makefile

--- opus/src/main.c
+++ opus/src/main.c
@@ -4,114 +4,104 @@
 #include <stdlib.h>
 #include <time.h>
 
-#include "pso.h"
 #include "logging.h"
+#include "pso.h"
 
 #include "timing.h"
 
 #define PROFILING
 
-double my_f(double const * const x, size_t d)
+double my_f(double const *const x, size_t d)
 {
-    return (x[0] - 2) * (x[0] - 2) + (x[1] - 5) * (x[1] - 5);
+  return (x[0] - 2) * (x[0] - 2) + (x[1] - 5) * (x[1] - 5);
 }
-
 
 int main(int argc, char **argv)
 {
-    if (argc > 1)
-    {
-        set_logging_directory(argv[1]);
-    }
+  if (argc > 1)
+  {
+    set_logging_directory(argv[1]);
+  }
 
 #ifdef PROFILING
-    double cycles;
+  double cycles;
 
-    profiling_hyperparameters* hyperparams = malloc(sizeof(profiling_hyperparameters));
+  profiling_hyperparameters *hyperparams =
+      malloc(sizeof(profiling_hyperparameters));
 
-    // the following should be fixed accross 'dimensions' comparisons 
-    // hyperparams->population_size = 4;
-    hyperparams->time_max = 10;
-    hyperparams->n_trials = 4;
+  // the following should be fixed accross 'dimensions' comparisons
+  // hyperparams->population_size = 4;
+  hyperparams->time_max = 10;
+  hyperparams->n_trials = 4;
 
-    hyperparams->dimensions = 2;
-    hyperparams->population_size = 4;
-    cycles = perf_counter(run_pso_f, hyperparams);
-    printf("%d, %lf\n", hyperparams->dimensions, cycles);
+  hyperparams->dimensions = 2;
+  hyperparams->population_size = 4;
+  cycles = perf_counter(run_pso_f, hyperparams);
+  printf("%d, %lf\n", hyperparams->dimensions, cycles);
 
-    // printf("Started profiling run_pso\n");
-    // for(int d=1; d<=30; ++d) {
-    //     hyperparams->dimensions = d;
-    //     hyperparams->population_size = d+1;
-    //     cycles = perf_counter(run_pso_f, hyperparams);
-    //     printf("%d, %lf\n", hyperparams->dimensions, cycles);
-    // }
+  // printf("Started profiling run_pso\n");
+  // for(int d=1; d<=30; ++d) {
+  //     hyperparams->dimensions = d;
+  //     hyperparams->population_size = d+1;
+  //     cycles = perf_counter(run_pso_f, hyperparams);
+  //     printf("%d, %lf\n", hyperparams->dimensions, cycles);
+  // }
 
-    // printf("\n\n\n\n");
-    
-    // printf("Started profiling plu_factorization\n");
-    // for(int N=10; N<=10000; N*=2) {
-    //     hyperparams->plu_matrix_side_length = N;
-    //     cycles = perf_counter(plu_factorization_f, hyperparams);
-    //     printf("%d, %lf\n", hyperparams->plu_matrix_side_length, cycles);
-    // }
+  // printf("\n\n\n\n");
 
-    // printf("Started profiling plu_solve\n");
-    // for(int N=10; N<=10000; N*=2) {
-    //     hyperparams->plu_matrix_side_length = N;
-    //     cycles = perf_counter(plu_solve_f, hyperparams);
-    //     printf("%d, %lf\n", hyperparams->plu_matrix_side_length, cycles);
-    // }
+  // printf("Started profiling plu_factorization\n");
+  // for(int N=10; N<=10000; N*=2) {
+  //     hyperparams->plu_matrix_side_length = N;
+  //     cycles = perf_counter(plu_factorization_f, hyperparams);
+  //     printf("%d, %lf\n", hyperparams->plu_matrix_side_length, cycles);
+  // }
 
-    // printf("\n\n\n\n");
+  // printf("Started profiling plu_solve\n");
+  // for(int N=10; N<=10000; N*=2) {
+  //     hyperparams->plu_matrix_side_length = N;
+  //     cycles = perf_counter(plu_solve_f, hyperparams);
+  //     printf("%d, %lf\n", hyperparams->plu_matrix_side_length, cycles);
+  // }
 
-    // printf("Started profiling fit_surrogate\n");
-    // for(int d=1; d<=30; ++d) {
-    //     hyperparams->dimensions = d;
-    //     hyperparams->population_size = d+1;
-    //     cycles = perf_counter(fit_surrogate_f, hyperparams);
-    //     printf("%d, %lf\n", hyperparams->dimensions, cycles);
-    // }
+  // printf("\n\n\n\n");
 
-    // printf("Started profiling fit_surrogate\n");
-    // cycles = perf_counter(fit_surrogate_f, hyperparams);
-    // printf("Number of cycles required for fit_surrogate: %lf\n", cycles);
-#else 
+  // printf("Started profiling fit_surrogate\n");
+  // for(int d=1; d<=30; ++d) {
+  //     hyperparams->dimensions = d;
+  //     hyperparams->population_size = d+1;
+  //     cycles = perf_counter(fit_surrogate_f, hyperparams);
+  //     printf("%d, %lf\n", hyperparams->dimensions, cycles);
+  // }
 
-    srand(clock());
+  // printf("Started profiling fit_surrogate\n");
+  // cycles = perf_counter(fit_surrogate_f, hyperparams);
+  // printf("Number of cycles required for fit_surrogate: %lf\n", cycles);
+#else
 
-    double inertia = 0.7;
-    double social = 1., cognition = 1.;
-    double local_refinement_box_size = 3.;
-    double min_minimizer_distance = 1.;
-    int dimensions = 2;
-    int population_size = 5;
-    int time_max = 100;
-    int n_trials = 5;
-    double bounds_low[2] = {-10., -10.};
-    double bounds_high[2] = {10., 10.};
-    double vmin[2] = {-10., -10.};
-    double vmax[2] = {10., 10.};
-    double initial_position_data[10] = {-1, 8, 5, -3, 5, 6, 7, 3, -9, -2};
-    double * initial_positions[5];
-    for(int i = 0; i < 5; i++)
-        initial_positions[i] = initial_position_data + 2 * i;
+  srand(clock());
 
-    run_pso(
-        &my_f,
-        inertia, social, cognition,
-        local_refinement_box_size,
-        min_minimizer_distance,
-        dimensions,
-        population_size, time_max, n_trials,
-        bounds_low, bounds_high,
-        vmin, vmax,
-        initial_positions       
-    );
+  double inertia = 0.7;
+  double social = 1., cognition = 1.;
+  double local_refinement_box_size = 3.;
+  double min_minimizer_distance = 1.;
+  int dimensions = 2;
+  int population_size = 5;
+  int time_max = 100;
+  int n_trials = 5;
+  double bounds_low[2] = {-10., -10.};
+  double bounds_high[2] = {10., 10.};
+  double vmin[2] = {-10., -10.};
+  double vmax[2] = {10., 10.};
+  double initial_position_data[10] = {-1, 8, 5, -3, 5, 6, 7, 3, -9, -2};
+  double *initial_positions[5];
+  for (int i = 0; i < 5; i++)
+    initial_positions[i] = initial_position_data + 2 * i;
 
-    //getchar();
+  run_pso(&my_f, inertia, social, cognition, local_refinement_box_size,
+          min_minimizer_distance, dimensions, population_size, time_max,
+          n_trials, bounds_low, bounds_high, vmin, vmax, initial_positions);
+
+  // getchar();
 
 #endif
-
-
 }
--- opus/src/pso.h
+++ opus/src/pso.h
@@ -8,103 +8,95 @@
 #include "rounding_bloom.h"
 #endif
 
-typedef double (*blackbox_fun)(double const * const, size_t d);
+typedef double (*blackbox_fun)(double const *const, size_t d);
 
 // PSO_X : pso::pso, t:int, i:int -> x_i(t):double*
 // PSO_FX : pso:pso, t:int, i:int -> f(x_i(t)):double
-#define PSO_X(pso, t, i) ( (pso)->x + ( (t) * (pso)->population_size  + (i) ) * (pso)->dimensions )
-#define PSO_FX(pso, t, i) (pso)->x_eval[ (t) * (pso)->population_size  + (i) ]
+#define PSO_X(pso, t, i)                                                       \
+  ((pso)->x + ((t) * (pso)->population_size + (i)) * (pso)->dimensions)
+#define PSO_FX(pso, t, i) (pso)->x_eval[(t) * (pso)->population_size + (i)]
 
 // PSO_V : pso::pso, i:int -> v_i:double*
-#define PSO_V(pso, i) ( (pso)->v + (i) * (pso)->dimensions )
+#define PSO_V(pso, i) ((pso)->v + (i) * (pso)->dimensions)
 
 // PSO_Y : pso::pso, i:int -> y_i:double*
 // PSO_FY : pso::pso, i:int -> f(y_i):double
-#define PSO_Y(pso, i) ( (pso)->y + (i) * (pso)->dimensions )
-#define PSO_FY(pso, i) (pso)->y_eval[ (t) * (pso)->population_size  + (i) ]
+#define PSO_Y(pso, i) ((pso)->y + (i) * (pso)->dimensions)
+#define PSO_FY(pso, i) (pso)->y_eval[(t) * (pso)->population_size + (i)]
 
 // PSO_PAST_REFINEMENT : pso::pso, k:int -> epsilon_k:double*
 // PSO_PAST_REFINEMENT_EVAL : pso:pso, k:int -> f(epsilon_k):double
-#define PSO_PAST_REFINEMENT(pso, k) ( (pso)->past_refinement_points + (k) * (pso)->dimensions )
+#define PSO_PAST_REFINEMENT(pso, k)                                            \
+  ((pso)->past_refinement_points + (k) * (pso)->dimensions)
 #define PSO_PAST_REFINEMENT_EVAL(pso, k) (pso)->past_refinement_points_eval[k]
 
 // for other inertia choices, see https://ieeexplore.ieee.org/document/6089659
 struct pso_data_constant_inertia
 {
-    blackbox_fun f;
-    // positions x_i, saved for all times
-    // i.e. PSO_X(pso, t, i) is the position vector x_i(t)
-    double * x;
-    // PSO_FX(pso, t, i) = f(x_i(t))
-    double * x_eval;
+  blackbox_fun f;
+  // positions x_i, saved for all times
+  // i.e. PSO_X(pso, t, i) is the position vector x_i(t)
+  double *x;
+  // PSO_FX(pso, t, i) = f(x_i(t))
+  double *x_eval;
 
-    // PSO_V(pso,i) = v_i
-    double * v;
+  // PSO_V(pso,i) = v_i
+  double *v;
 
-    // PSO_Y(pso,i) := y_i := best position ever recorded for particle i
-    double * y;
-    // PSO_FY(pso,i) := f(y_i)
-    double * y_eval;
-    
-    // ŷ = best position ever recorded over all particles
-    // here a pointer to one of the y_i or in past_refinement_points
-    double * y_hat;
+  // PSO_Y(pso,i) := y_i := best position ever recorded for particle i
+  double *y;
+  // PSO_FY(pso,i) := f(y_i)
+  double *y_eval;
 
-    // Keep track of local refinement points.
-    // max lenght of list = tmax
-    // current length  = n_past_refinement_points
-    double * past_refinement_points;
-    double * past_refinement_points_eval; 
+  // ŷ = best position ever recorded over all particles
+  // here a pointer to one of the y_i or in past_refinement_points
+  double *y_hat;
 
+  // Keep track of local refinement points.
+  // max lenght of list = tmax
+  // current length  = n_past_refinement_points
+  double *past_refinement_points;
+  double *past_refinement_points_eval;
 
-    double * bound_low;
-    double * bound_high;
-    double * vmin;
-    double * vmax;
+  double *bound_low;
+  double *bound_high;
+  double *vmin;
+  double *vmax;
 
+  // list of idx of distinct
+  size_t *x_distinct;
+  size_t x_distinct_s;
+#ifdef USE_ROUNDING_BLOOM_FILTER
+  struct rounding_bloom *bloom;
+#endif
 
-    // list of idx of distinct
-    size_t * x_distinct;
-    size_t x_distinct_s;
-    #ifdef USE_ROUNDING_BLOOM_FILTER
-    struct rounding_bloom * bloom;
-    #endif
+  // parameters of the surrogate
+  double *lambda;
+  double *p;
 
+  double inertia;
+  double social;
+  double cognition;
 
-    // parameters of the surrogate
-    double * lambda;
-    double * p;
+  double local_refinement_box_size;
+  double min_minimizer_distance;
 
-    double inertia;
-    double social;
-    double cognition;
+  double y_hat_eval;
 
-    double local_refinement_box_size;
-    double min_minimizer_distance;
+  int dimensions;
+  int population_size;
+  int n_trials;
 
-    double y_hat_eval;
-    
-    int dimensions;
-    int population_size;
-    int n_trials;
+  int n_past_refinement_points;
 
-    int n_past_refinement_points;
-
-    int time_max;
-    int time;
+  int time_max;
+  int time;
 };
 
-void run_pso(
-    blackbox_fun f,
-    double inertia,
-    double social, double cognition,
-    double local_refinement_box_size,
-    double min_minimizer_distance,
-    int dimensions,
-    int population_size, int time_max, int n_trials,
-    double * bounds_low, double * bounds_high,
-    double * vmin, double * vmax,
-    double ** initial_positions
-);
+void run_pso(blackbox_fun f, double inertia, double social, double cognition,
+             double local_refinement_box_size, double min_minimizer_distance,
+             int dimensions, int population_size, int time_max, int n_trials,
+             double *bounds_low, double *bounds_high, double *vmin,
+             double *vmax, double **initial_positions);
 
-int fit_surrogate(struct pso_data_constant_inertia * pso);
+int fit_surrogate(struct pso_data_constant_inertia *pso);
